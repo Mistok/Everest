4332a491314b38938323b4acc64c3eef6dd6f16f
Mistok <mistreated@ukr.net>
2017-11-09 16:16:27 +0200

    100% done

--- js/hidden_menu_active.js
+++ js/hidden_menu_active.js
@@ -1,32 +1,10 @@
-
 /*show hidden menu button*/
-
 jQuery(document).ready(function($) {
 			$('.hidden_menu_button').click(function(event) {
 				$('.main_content,.menu_list,.main_menu').toggleClass('active');
 			});
 		});
-
-/* search form script*/
-
-jQuery(document).ready(function($) {
-				$('.search_form').click(function(event) {
-					$('.search').toggleClass('active');
-				});
-			});
-
-/*slick slider setting */
-
-$('.sl').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-});
-
 /*close hidden menu button */
-
 jQuery(document).ready(function($) {
 			$('.close').click(function(event) {
 				$('.main_content,.menu_list,.main_menu').removeClass('active');
@@ -34,9 +12,7 @@ jQuery(document).ready(function($) {
 		});
 
 /* calendar script */
-
 $("#calendar_in,#calendar_out").datetimepicker({
 				timepicker:false,
  				format:'d.m.Y'
 			});
-
